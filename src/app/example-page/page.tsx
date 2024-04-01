6c23ef12fe0060a21e7d4ba09d6f9348f08ae1fd
feeermino <87814305+knnbranding@users.noreply.github.com>
2024-04-01 15:47:39 -0300

    template adjustments pt 1

---
+++ src/app/example-page/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const Example = () => {
+  return (
+    <div className={styles.example}>
+      <h1>Example</h1>
+    </div>
+  );
+};
+
+export default Example;
